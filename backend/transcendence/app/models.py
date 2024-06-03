95a6caa1f2d29bcb61699b224c03b9a3ef8bb46e
Carlos Rocha <carlos.rocha01@gmail.com>
2024-06-03 16:37:29 -0300

    feat(models): add User model to handle user information

--- backend/transcendence/app/models.py
+++ backend/transcendence/app/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+# Create users table
+class User(models.Model):
+	"""
+	Represents a user in the system.
+
+	Attributes:
+		name (str): The name of the user. max_length=100
+		nick (str): The nickname of the user. max_length=50, unique(unique garanties that there are no two users with the same nickname)
+		score (int): The score of the user. default=0 (if not specified, the score will be 0)
+		password (str): The password of the user. max_length=255(sha256 hash of the password)
+	"""
+
+	name = models.CharField(max_length=100)
+	nick = models.CharField(max_length=50, unique=True)
+	score = models.IntegerField(default=0)
+	password = models.CharField(max_length=255)
+
+	def __str__(self):
+		return self.name
