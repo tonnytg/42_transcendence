77f42d3c8b607f9bd83fd97d9194bc0904538b75
Antonio Thomacelli <tonnytg@gmail.com>
2024-06-06 22:15:09 -0300

    feat: finish spa with login 42

--- backend/transcendence/transcendence/settings.py
+++ backend/transcendence/transcendence/settings.py
@@ -131,6 +131,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+# Load css
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
 
 # Where to store static files
 STATIC_ROOT = os.path.join(BASE_DIR, 'public', 'static')
