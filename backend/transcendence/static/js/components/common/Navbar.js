84d4bb8b263d2dc92a3a7d494770364eb539cd5e
Marcelo Magalhães <marcelomagalhaes@protonmail.com>
2024-06-22 10:46:56 -0300

    refactor(navbar): use templates for improved efficiecy

--- backend/transcendence/static/js/components/common/Navbar.js
+++ backend/transcendence/static/js/components/common/Navbar.js
@@ -1,47 +1,60 @@
+const template = document.createElement('template');
+template.innerHTML = `
+    <style>
+        .nav-masthead {
+            display: flex;
+            justify-content: center;
+        }
+
+        .nav-link {
+            font-weight: bold;
+            padding: 0.5rem 1rem;
+            color: white;
+        }
+    </style>
+    <nav class="nav nav-masthead">
+        <a data-link class="nav-link active" aria-current="page" href="/">Home</a>
+        <a data-link class="nav-link" href="/game">Game</a>
+        <a data-link class="nav-link" href="/invite">Friends</a>
+        <a data-link class="nav-link" href="/chat">Chat</a>
+        <a data-link class="nav-link" href="/dashboard">Dashboard</a>
+        <a data-link class="nav-link" href="#">Profile</a>
+    </nav>
+`;
+
 class NavBar extends HTMLElement {
     constructor() {
         super();
         this.attachShadow({ mode: 'open' });
+        this.shadowRoot.appendChild(template.content.cloneNode(true));
+        this.links = this.shadowRoot.querySelectorAll('[data-link]');
+        this.handleLinkClick = this.handleLinkClick.bind(this);
     }
 
     connectedCallback() {
-        this.render();
         this.addLinkEvents();
     }
 
-    render() {
-        this.shadowRoot.innerHTML = `
-            <style>
-                .nav-masthead {
-                    display: flex;
-                    justify-content: center;
-                }
-
-                .nav-link {
-                    font-weight: bold;
-                    padding: 0.5rem 1rem;
-                }
-            </style>
-            <nav class="nav nav-masthead">
-                <a data-link class="nav-link active" aria-current="page" href="/">Home</a>
-                <a data-link class="nav-link" href="/game">Game</a>
-                <a data-link class="nav-link" href="/invite">Friends</a>
-                <a data-link class="nav-link" href="/chat">Chat</a>
-                <a data-link class="nav-link" href="/dashboard">Dashboard</a>
-                <a data-link class="nav-link" href="#">Profile</a>
-            </nav>
-        `;
+    disconnectedCallback() {
+        this.removeLinkEvents();
     }
 
     addLinkEvents() {
-        const links = this.shadowRoot.querySelectorAll('[data-link]');
-        links.forEach(link => {
-            link.addEventListener('click', (event) => {
-                event.preventDefault();
-                const path = link.getAttribute('href');
-                window.router.navigateTo(path);
-            });
+        this.links.forEach(link => {
+            link.addEventListener('click', this.handleLinkClick);
         });
+    }
+
+    removeLinkEvents() {
+        this.links.forEach(link => {
+            link.removeEventListener('click', this.handleLinkClick);
+        });
+    }
+
+    handleLinkClick(event) {
+        event.preventDefault();
+        const path = event.currentTarget.getAttribute('href');
+        window.router.navigateTo(path);
     }
 }
 
